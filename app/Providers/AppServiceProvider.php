8b4c8fb6b520d980406d86a5161c84ad4e69a644
Logan Hsia <logan@hsia.us>
2023-10-27 09:26:30 -0400

    feat(ocp_start): binding repository interfaces

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Repositories\Contracts\ProductRepositoryInterface;
+use App\Repositories\Contracts\StockRepositoryInterface;
+use App\Repositories\MysqlProductRepository;
+use App\Repositories\MysqlStockRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(ProductRepositoryInterface::class, MysqlProductRepository::class);
+        $this->app->bind(StockRepositoryInterface::class, MysqlStockRepository::class);
     }
 
     /**
