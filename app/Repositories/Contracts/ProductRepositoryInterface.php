6166685e8499abbc38f3aa6ffb06385be0d86a8c
Logan Hsia <logan@hsia.us>
2023-10-27 09:15:49 -0400

    feat(ocp_start): created product and stock repository interfaces. changed product and stock repositories to mysql repositories

---
+++ app/Repositories/Contracts/ProductRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+
+
+interface ProductRepositoryInterface
+{
+    /**
+     * @param $product_id
+     * @return mixed
+     */
+    public function getById($product_id);
+}
