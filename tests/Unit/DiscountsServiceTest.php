0a120561f4f9f428f0a014caf0e77f52d427f3ee
Logan Hsia <logan@hsia.us>
2023-10-30 16:30:37 -0400

    feat(dip_start): refactored DiscountServiceTest to adhere to newer laravel version

--- tests/Unit/DiscountsServiceTest.php
+++ tests/Unit/DiscountsServiceTest.php
@@ -7,17 +7,18 @@ use App\Patterns\Discounts\FifttyPercentDiscount;
 use App\Patterns\Discounts\TwentyPercentDiscount;
 use App\Services\DiscountService;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 
 class DiscountsServiceTest extends TestCase
 {
+    use WithFaker;
 
     /** @test */
     public function it_applies_20_percent_discount_correctly(): void
     {
-        $product = factory(Product::class)->make([
-            'price'  => 40
+        $product = Product::factory()->make([
+            'price' => 40
         ]);
 
         $discountService = new DiscountService(new TwentyPercentDiscount);
@@ -25,14 +26,13 @@ class DiscountsServiceTest extends TestCase
 
         // 32
         $this->assertSame(32, intval($total));
-
     }
 
     /** @test */
     public function it_applies_50_percent_discount_correctly(): void
     {
-        $product = factory(Product::class)->make([
-            'price'  => 10
+        $product = Product::factory()->make([
+            'price' => 10
         ]);
 
         $discountService = new DiscountService(new FifttyPercentDiscount);
@@ -45,15 +45,13 @@ class DiscountsServiceTest extends TestCase
     /** @test */
     public function it_applies_20_percent_discount_correctly_if_service_is_instantiated_through_make_method(): void
     {
-        $product = factory(Product::class)->make([
-            'price'  => 40
+        $product = Product::factory()->make([
+            'price' => 40
         ]);
 
-//        $discountService = new DiscountService(new TwentyPercentDiscount);
         $total = DiscountService::make(new TwentyPercentDiscount)->with($product)->apply();
 
         // 32
         $this->assertSame(32, intval($total));
-
     }
 }
